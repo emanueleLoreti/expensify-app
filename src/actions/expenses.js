d6c8020287b522de78fe406796409666bd2dc02a
Andrea Verdelocco <a.verdelocco@2hire.io>
2020-05-26 16:48:41 +0200

    Fetxh expenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,5 @@
 import database from "../firebase/firebase";
+import expenses from "../tests/fixtures/expenses";
 
 export const addExpense = (expense) => ({
   type: "ADD_EXPENSE",
@@ -37,3 +38,23 @@ export const editExpense = (id, updates) => ({
   id,
   updates,
 });
+
+export const setExpenses = (expenses) => ({
+  type: "SET_EXPENSES",
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref("expenses").once("value").then((snapshot) => {
+      const expenses = [];
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
